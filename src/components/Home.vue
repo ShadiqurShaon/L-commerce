e6fd5f4ab6609aaab7f3cdcfa643dc4c73a73274
shadiqur <shadiqurshaon@gmail.com>
2018-07-11 22:42:55 +0600

    add slider and popular product pannel

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,63 @@
 <template>
   <div class="home-page">
     <div class="banner">
-      <div class="container">
-        <h1 class="logo-font">conduit</h1>
-        <p>A place to share your knowledge.</p>
+      
+
+  <div class="row">
+    <div class="col-sm-4" style="background-color:lightgreen">
+      <div class="popularProduct" style="text-align:center;padding-top:5px;">
+          <h2 style="color:#003300">Popular Product</h2>
+  <div class="panel panel-default">
+    <div class="panel-body">A Basic Panel</div>
+  </div>
       </div>
+  
+    </div>
+
+     <div class="col-sm-8">
+       <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="../assets/screenshot-www.comparemymobile.com-2018.07.09-13-03-33.png" alt="Los Angeles" style="width:100%;">
+      </div>
+
+      <div class="item">
+        <img src="../assets/screenshot-www.dxomark.com-2018.07.09-13-04-03.png" alt="Los Angeles" style="width:100%;">
+        
+      </div>
+    
+      <div class="item">
+        <img src="../assets/screenshot-www.eventsclothing.co.nz-2018.07.09-12-59-33.png" alt="Los Angeles" style="width:100%;">
+        
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+    </div>
+  </div>
+ 
+
+
+
+
+      
     </div>
     <div class="container page">
       <div class="row">
@@ -60,6 +113,10 @@
 
 <script>
   import { mapGetters } from 'vuex'
+  import Vue from 'vue'
+  import VueAgile from 'vue-agile'
+
+      Vue.use(VueAgile)
   // import RwvTag from '@/components/VTag'
   // import { FETCH_TAGS } from '@/store/actions.type'
 
@@ -70,6 +127,9 @@
     },
     mounted () {
       // this.$store.dispatch(FETCH_TAGS)
+     $('.carousel').carousel({
+  interval: 3000
+})
     },
     // computed: {
     //   ...mapGetters([
@@ -82,3 +142,18 @@
     // }
   }
 </script>
+
+<style scoped>
+ .carousel-inner > .item > img{
+ 
+   height: 400px;
+ }
+  /* .carousel-inner > .item > a > img {
+      width: 70%;
+      margin: auto;
+  } */
+</style>
+
+
+
+
