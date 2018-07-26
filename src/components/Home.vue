798356dcb97901b41468993b07224ef7a5ee7985
shadiqur <shadiqurshaon@gmail.com>
2018-07-26 17:07:53 +0600

    finish homepage carusal iteam

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,12 +3,12 @@
 
 <div class="containner">
     <div class="row">
-      <div class="col l4 m6 s12" style="background-color:gray">
+      <div class="col l4 m6 s12 card" >
         <div class="carousel" id="first">
-          <h3>Popular product</h3>
+          <p class="card-title">Popular Product</p>
 
       <router-link  :to="{name: 'home'}" class="carousel-item" > 
-        <img src="../assets/0084030.jpg" alt="Los Angeles">
+        <img src="../assets/0084030.jpg" alt="Los Angeles">name
          </router-link>
          <router-link  :to="{name: 'home'}" class="carousel-item" > 
         <img src="../assets/0084030.jpg" alt="Los Angeles">
@@ -26,8 +26,8 @@
 
       <div class="col l8 m6 s12">
        
-           <div class="carousel carousel-slider" id="second">
-          <h3>Popular product</h3>
+           <div class="carousel carousel-slider z-depth-1" id="second">
+          
 
       <router-link  :to="{name: 'home'}" class="carousel-item" > 
         <img src="../assets/0084030.jpg" alt="Los Angeles">
@@ -189,7 +189,7 @@
             <!-- </div>
           </div>
         </div>
-      </div>
+      </div>  
     </div> -->
 
 </template>
@@ -209,31 +209,33 @@ export default {
   data() {
     return {
       Products: [
-        { id:3,
-          image:"0081286.jpg",
+        {
+          id: 3,
+          image: "0081286.jpg",
           name: "CHECK PRINT SHIRT",
           price: 110
         },
-        {id:2,
-          image:"0081286.jpg",
+        {
+          id: 2,
+          image: "0081286.jpg",
           name: "GLORIA HIGH LOGO SNEAKER",
           price: 91
         },
-        {id:1,
-          image:
-            "0081286.jpg",
+        {
+          id: 1,
+          image: "0081286.jpg",
           name: "CATE RIGID BAG",
           price: 94.5
         },
-        {id:4,
-          image:
-            "0081286.jpg",
+        {
+          id: 4,
+          image: "0081286.jpg",
           name: "GUESS CONNECT WATCH",
           price: 438.9
         },
-        {id:6,
-          image:
-            "0081286.jpg",
+        {
+          id: 6,
+          image: "0081286.jpg",
           name: "'70s RETRO GLAM KEFIAH",
           price: 20
         }
@@ -244,28 +246,66 @@ export default {
     ProductPreview: ProductPreview
   },
   mounted() {
-    
+    $("#second").carousel({
+      full_width: true,
+        dist: 0,
+    padding: 0,
    
-    $('#second').carousel({
-    full_width: true
-  });
-   $('#first').carousel();
+    indicators: true,
+    duration: 4000,
       
- 
-  
+    });
+    autoplay();
+    function autoplay() {
+      $("#second").carousel("next");
+      setTimeout(autoplay, 4000);
+    }
+
+    $("#first").carousel();
   }
- 
 };
 </script>
 
 <style scoped>
-/* .carousel-inner > .item > img {
-  height: 400px;
-} */
-/* .carousel-inner > .item > a > img {
-      width: 70%;
-      margin: auto;
-  } */
+.containner {
+  margin: 10px;
+  text-align: center;
+}
+.containner #first {
+  height: 300px;
+}
+.containner #first .carousel-item {
+  height: 150px;
+  width: 197px;
+}
+.containner #first img {
+  height: 150px;
+  width: 197px;
+  margin-top: 40px;
+}
+#first p {
+  display: block;
+  border: 1px soli;
+  width: 100%;
+  font-size: 20px;
+  background-color: lightgrey;
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+}
+.containner #second {
+  height: 300px !important;
+  margin-top: 7px;
+  /* border:1px solid gray; */
+  /* background-color: lightgray; */
+}
+#second .carousel-item {
+  width: 100%;
+  height: 100%;
+  margin: 10px;
+}
+#second .carousel-item img {
+  margin-left: -20px;
+  padding: 10px;
+}
 </style>
 
 
